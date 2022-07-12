ca8ae53703f84b9eafef7305f55565a8ecb3eaba
rhymn <david@pnd.se>
2022-07-12 14:35:43 +0200

    no base and no projectName

--- kaddio/docusaurus.config.js
+++ kaddio/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://documentaion.onrender.com',
-  baseUrl: '/kaddio',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kaddio', // Usually your GitHub org/user name.
-  projectName: 'doc', // Usually your repo name.
+  // projectName: 'doc', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
